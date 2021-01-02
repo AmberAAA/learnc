c1032e4bb17a030d77d27e4bf792acaef034688a
Amber <anborong@outlook.com>
2021-01-02 13:16:31 +0800

    var log

--- learnc-day1/hello.cpp
+++ learnc-day1/hello.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
-using namespace std;
+#define Day 7
+// using namespace std;
+
+// 单行注释
+
+/*
+	行内注释
+*/
 
 int main()
 {
+	const int v = 3;
 
-	cout << "Hello World!" << endl;
+	int a = 3;
+
+	int c = Day;
+
+	std::cout << "一周一共有：" << Day << std::endl;
+
+	std::cout << "a = " << a << std::endl;
+
+	std::cout << "Hello World!" << std::endl;
 
 	system("pause");
-
-	return 0;
 }
